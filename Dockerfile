bf4ff8d6a794f33a12ba23c6592978b9d3d57c78
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-08-16 00:48:07 -0300

    [BUGGY]: docker build porecesses integration with gradle

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 # Use the official image as a parent image
 FROM openjdk:17 as build
 
+# Set the argument for the version tag
+ARG TAG="0.0.2"
+
 # Set the location of the app
 WORKDIR /usr/app
 
 # Copy the local pre-built jar to the container
-COPY build/libs/server-manager-0.0.1-SNAPSHOT.jar /usr/app/app.jar
+COPY build/libs/server-manager-${TAG}-SNAPSHOT.jar /usr/app/app.jar
 
 # Make port 8080 available to the world outside this container
 EXPOSE 8080
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.tms"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -23,14 +23,16 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("org.postgresql:postgresql")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.2")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.1.0")
+	compileOnly("org.projectlombok:lombok:1.18.26")
+	developmentOnly("org.springframework.boot:spring-boot-devtools:3.0.4")
+	runtimeOnly("org.postgresql:postgresql:42.5.4")
+	annotationProcessor("org.projectlombok:lombok:1.18.26")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dockerCompose {
@@ -42,8 +44,26 @@ tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
+tasks.register("updateDockerCompose") {
+	doLast {
+		val dockerComposeFile = file("docker-compose.yml")
+		val content = dockerComposeFile.readText()
+		val updatedContent = content.replace("__TAG__", project.version.toString())
+		dockerComposeFile.writeText(updatedContent)
+	}
+}
+
+tasks.register("dockerBuild") {
+	dependsOn("updateDockerCompose") // Certifica-se de que a tarefa updateDockerCompose seja executada primeiro
+	doLast {
+		exec {
+			commandLine("docker", "build", "--build-arg", "TAG=${project.version}", "-t", "server-manager-app:${project.version}", ".")
+		}
+	}
+}
+
 tasks.register("myComposeUp") {
-	dependsOn("dockerComposeUp")
+	dependsOn("dockerBuild", "dockerComposeUp") // Certifica-se de que a tarefa dockerBuild seja executada antes de dockerComposeUp
 }
 
 tasks.register("myComposeDown") {
