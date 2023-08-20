745aa90e5624bfafa8937d122980abd5215df1a3
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-08-20 12:24:22 -0300

    fix: folder reestructure

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM openjdk:17
-VOLUME /tmp
-EXPOSE 8080
-ARG TAG=latest
-ARG VERSION=0.0.1-SNAPSHOT
-ARG JAR_FILE=build/libs/server-manager-${VERSION}.jar
-ADD ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/io/tms/servermanager/repo/ServerRepo.java
+++
@@ -1,8 +0,0 @@
-package io.tms.servermanager.repo;
-
-import io.tms.servermanager.model.Server;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServerRepo extends JpaRepository<Server, Long> {
-    Server findByIpAddress(String ipAddress);
-}
