60d3ccc4d844f6793dd1b284d7335a688e956cbd
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-08-29 11:24:02 -0300

    buggy: still working on the CORS configuration

--- backend-server-manager/src/main/java/io/tms/servermanager/ServerApplication.java
+++ backend-server-manager/src/main/java/io/tms/servermanager/ServerApplication.java
@@ -3,13 +3,10 @@ package io.tms.servermanager;
 import io.tms.servermanager.enumeration.Status;
 import io.tms.servermanager.model.Server;
 import io.tms.servermanager.repo.ServerRepo;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
@@ -62,19 +59,4 @@ public class ServerApplication {
 		};
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5500", "http://localhost:3000"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-			"Accept", "Jwt-Token", "Authorization", "Origin", "Accept", "X-Requested-With",
-			"Access-Control-Request-Method", "Access-Control-Request-Method"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
-			"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS"));
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter();
-	}
 }
